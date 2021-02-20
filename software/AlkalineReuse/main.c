6956e0858a7b76bd31b93c6d7ab35f43f7f1bf39
sylvain <sylvain.noiry@hotmail.fr>
2021-02-20 17:53:16 +0100

    ready to publish
    v1.0

--- software/AlkalineReuse/main.c
+++ software/AlkalineReuse/main.c
@@ -6,12 +6,16 @@
  */
 
 #define F_CPU (1000000UL) // 1 MHz
+
 #define MinVoltage 71 // min voltage for charging : 1.20 V
 #define MaxVoltage 98 // max voltage for charging : 1.65 V
-#define MinTemperature 67 // min temperature for charging (basically prevent from sensor error) : 0°C
-#define MaxTemperature 162 // max temperature for charging : 40°C
+#define MinTemperature 67 // min temperature for charging (basically prevent from sensor error) : 0?C
+#define MaxTemperature 162 // max temperature for charging : 40?C
 #define voltageHysteresis 2 // tolerance for voltage hysteresis
 #define temperatureHysteresis 2 // tolerance for temperature hysteresis
+
+#define charging_time 120
+#define idle_time 50
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -64,7 +68,6 @@ void check() {
 			}
 		}
 	}
-
 	// check temperature
 	ADMUX = 0b00000001; // ADC multiplexer PB1
 	ADCSRA |= (1 << ADSC); // start conversion
@@ -112,17 +115,18 @@ void check() {
 int main(void)
 {
 	DDRB = 0b00000100; // set inputs and outputs
-  PORTB = 0b0000000; // outputs set to LOW and input set to h-Z
+	PORTB = 0b0000000; // outputs set to LOW and input set to h-Z
 	DIDR0 = 0b00000011; // disable digital input for ADC inputs
+	ADCSRA = 0b10000011; // enable ADC and set ADC clock at 125 kHz
     while (1)
     {
 		check();
 		if(chargeState == 1){
 			for(int i = 0; i < 160; i++){
 				 PORTB = 0b0000100;
-				 _delay_ms(120);
+				 _delay_ms(idle_time);
 				 PORTB = 0b0000000;
-				 _delay_ms(5);
+				 _delay_ms(charging_time);
 			}
 		}
 		else{
@@ -130,3 +134,4 @@ int main(void)
 		}
     }
 }
+
